3e4343f475f1dea09b5bc71f161119c82f3d4d58
Jamie Norrish <jamie@artefact.org.nz>
2013-03-01 17:28:41 +1300

    Removed unwanted import of draft code.

--- tacl/__init__.py
+++ tacl/__init__.py
@@ -1,7 +1,6 @@
 from .catalogue import Catalogue
 from .corpus import Corpus
 from .db_manager import DBManager
-from .graph import TextRelevanceProportionGraph
 from .report import Report
 from .stripper import Stripper
 from .text import Text
