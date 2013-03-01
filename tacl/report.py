a3f667ad0df213ea89b022f759cb576818bd2abb
Jamie Norrish <jamie@artefact.org.nz>
2013-03-01 16:15:30 +1300

    Reworked reduce report method in order to be correct.

--- tacl/report.py
+++ tacl/report.py
@@ -4,12 +4,17 @@ import csv
 import io
 import logging
 
+from .tokenizer import tokenizer
+
 
 COUNT_INDEX = 3
 FILENAME_INDEX = 2
 LABEL_INDEX = 4
 NGRAM_INDEX = 0
 SIZE_INDEX = 1
+
+HOMOGENOUS_KEY = 'homogenous'
+FILENAMES_KEY = 'filenames'
 
 
 class Report:
@@ -30,6 +35,24 @@ class Report:
         output_fh.seek(0)
         return output_fh
 
+    def _generate_substrings (self, ngram, size):
+        """Returns a list of all substrings of `ngram`.
+
+        :param ngram: n-gram to generate substrings of
+        :type ngram: `str`
+        :param size: size of `ngram`
+        :type size: `int`
+        :rtype: `list`
+
+        """
+        substrings = []
+        tokens = tokenizer.tokenize(ngram)
+        for n in range(1, size):
+            count = max(0, len(tokens) - n + 1)
+            ngrams = [''.join(tokens[i:i+n]) for i in range(count)]
+            substrings.extend(ngrams)
+        return substrings
+
     def prune (self, minimum=None, maximum=None):
         """Removes results rows who n-gram size are outside the range
         specified by `minimum` and `maximum`.
@@ -40,6 +63,7 @@ class Report:
         :type maximum: `int`
 
         """
+        logging.info('Pruning the n-grams')
         new_rows = []
         for row in self._rows:
             size = int(row[SIZE_INDEX])
@@ -50,25 +74,28 @@ class Report:
             new_rows.append(row)
         self._rows = new_rows
 
-    def _reduce_ngram (self, data, ngram, size):
-        """Lowers the counts of `ngram` in `data` if it forms part of
-        a larger n-gram.
+    def _reduce_by_ngram (self, data, ngram):
+        """Lowers the counts of all n-grams in `data` that are
+        substrings of `ngram` by `ngram`\'s count.
 
         Modifies `data` in place.
 
-        :param data: row data dictionary
+        :param data: row data dictionary for the current text
         :type data: `dict`
         :param ngram: n-gram being reduced
         :type ngram: `str`
-        :param size: size of `ngram`
-        :type size: `int`
 
         """
-        larger_size = size + 1
-        for larger_ngram in data.get(larger_size, []):
-            if ngram in larger_ngram:
-                for filename, count in data[larger_size][larger_ngram].items():
-                    data[size][ngram][filename] -= count
+        # Find all substrings of `ngram` and reduce their count by the
+        # count of `ngram`. Substrings may not exist in `data`.
+        count = data[ngram]['count']
+        for substring in self._generate_substrings(ngram, data[ngram]['size']):
+            try:
+                substring_data = data[substring]
+            except KeyError:
+                continue
+            else:
+                substring_data['count'] -= count
 
     def reduce (self):
         """Removes results rows whose n-grams are contained in larger
@@ -78,24 +105,28 @@ class Report:
         labels = {}
         # Derive a convenient data structure from the rows.
         for row in self._rows:
-            size = data.setdefault(int(row[SIZE_INDEX]), {})
-            ngram = size.setdefault(row[NGRAM_INDEX], {})
             filename = row[FILENAME_INDEX]
-            ngram[filename] = int(row[COUNT_INDEX])
             labels[filename] = row[LABEL_INDEX]
-        sizes = list(data.keys())
-        sizes.sort()
-        for size in sizes:
-            for ngram in data[size]:
-                self._reduce_ngram(data, ngram, size)
+            text_data = data.setdefault(filename, {})
+            text_data[row[NGRAM_INDEX]] = {
+                'count': int(row[COUNT_INDEX]),
+                'size': int(row[SIZE_INDEX])}
+        for filename, text_data in data.items():
+            ngrams = list(text_data.keys())
+            ngrams.sort(key=lambda ngram: text_data[ngram]['size'],
+                        reverse=True)
+            for ngram in ngrams:
+                if text_data[ngram]['count'] > 0:
+                    self._reduce_by_ngram(text_data, ngram)
         # Recreate rows from the modified data structure.
         self._rows = []
-        for size, ngrams in data.items():
-            for ngram, filenames in ngrams.items():
-                for filename, count in filenames.items():
-                    if count > 0:
-                        self._rows.append(
-                            [ngram, size, filename, count, labels[filename]])
+        for filename, text_data in data.items():
+            for ngram, ngram_data in text_data.items():
+                count = ngram_data['count']
+                if count > 0:
+                    self._rows.append(
+                        [ngram, ngram_data['size'], filename, count,
+                         labels[filename]])
 
     def remove_label (self, label):
         logging.info('Removing label "{}"'.format(label))
@@ -106,7 +137,7 @@ class Report:
                 count += 1
             else:
                 new_rows.append(row)
-        logging.info('Removed {} results'.format(count))
+        logging.info('Removed {} labelled results'.format(count))
         self._rows = new_rows
 
     def sort (self):
--- tests/report_test.py
+++ tests/report_test.py
@@ -60,14 +60,78 @@ class ReportTestCase (unittest.TestCase):
             ['AB', '2', 'a', '4', 'A'], ['ABC', '3', 'a', '2', 'A'],
             ['ABD', '3', 'a', '1', 'A'], ['ABCD', '4', 'a', '2', 'A'],
             ['AB', '2', 'b', '2', 'A'], ['ABC', '3', 'b', '2', 'A'])
-        fh = self._create_csv(input_data)
-        report = tacl.Report(fh)
-        report.reduce()
         expected_rows = set(
             (('AB', '2', 'a', '1', 'A'), ('ABD', '3', 'a', '1', 'A'),
              ('ABCD', '4', 'a', '2', 'A'), ('ABC', '3', 'b', '2', 'A')))
-        actual_rows = self._get_rows_from_csv(report.csv())
+        actual_rows = self._perform_reduce(input_data)
         self.assertEqual(actual_rows, expected_rows)
+        # Overlapping n-grams are trickier. Take the intersection of
+        # two texts:
+        #     ABABABCABDA and ABABABEABFA
+        input_data = (
+            ['ABABAB', '6', 'text1', '1', 'C1'],
+            ['ABABA', '5', 'text1', '1', 'C1'],
+            ['BABAB', '5', 'text1', '1', 'C1'],
+            ['ABAB', '4', 'text1', '2', 'C1'],
+            ['BABA', '4', 'text1', '1', 'C1'],
+            ['ABA', '3', 'text1', '2', 'C1'],
+            ['BAB', '3', 'text1', '2', 'C1'],
+            ['AB', '2', 'text1', '4', 'C1'],
+            ['BA', '2', 'text1', '2', 'C1'],
+            ['A', '1', 'text1', '5', 'C1'],
+            ['B', '1', 'text1', '4', 'C1'])
+        expected_rows = set(
+            (('ABABAB', '6', 'text1', '1', 'C1'),
+             ('AB', '2', 'text1', '1', 'C1'),
+             ('A', '1', 'text1', '1', 'C1')))
+        actual_rows = self._perform_reduce(input_data)
+        self.assertEqual(actual_rows, expected_rows)
+        # An n-gram that consists of a single repeated token is a form
+        # of overlapping. Take the intersection of two texts:
+        #     AAAAABAAAACAAADAAEA and AAAAAFAAAAGAAAHAAIA
+        input_data = (
+            ['AAAAA', '5', 'text1', '1', 'C1'],
+            ['AAAA', '4', 'text1', '3', 'C1'],
+            ['AAA', '3', 'text1', '6', 'C1'],
+            ['AA', '2', 'text1', '10', 'C1'],
+            ['A', '1', 'text1', '15', 'C1'],
+            ['AAAAA', '5', 'text2', '1', 'C2'],
+            ['AAAA', '4', 'text2', '3', 'C2'],
+            ['AAA', '3', 'text2', '6', 'C2'],
+            ['AA', '2', 'text2', '10', 'C2'],
+            ['A', '1', 'text2', '15', 'C2'])
+        expected_rows = set(
+            (('AAAAA', '5', 'text1', '1', 'C1'),
+             ('AAAA', '4', 'text1', '1', 'C1'),
+             ('AAA', '3', 'text1', '1', 'C1'),
+             ('AA', '2', 'text1', '1', 'C1'),
+             ('A', '1', 'text1', '1', 'C1'),
+             ('AAAAA', '5', 'text2', '1', 'C2'),
+             ('AAAA', '4', 'text2', '1', 'C2'),
+             ('AAA', '3', 'text2', '1', 'C2'),
+             ('AA', '2', 'text2', '1', 'C2'),
+             ('A', '1', 'text2', '1', 'C2')))
+        actual_rows = self._perform_reduce(input_data)
+        self.assertEqual(actual_rows, expected_rows)
+        # If the token is more than a single character, the case is
+        # even more pathological. [...] is a single token.
+        #     [A][A][A]B[A][A]C[A] and [A][A][A]D[A][A]E[A]
+        input_data = (
+            ['[A][A][A]', '3', 'text1', '1', 'C1'],
+            ['[A][A]', '2', 'text1', '3', 'C1'],
+            ['[A]', '1', 'text1', '6', 'C1'])
+        expected_rows = set(
+            (('[A][A][A]', '3', 'text1', '1', 'C1'),
+             ('[A][A]', '2', 'text1', '1', 'C1'),
+             ('[A]', '1', 'text1', '1', 'C1')))
+        actual_rows = self._perform_reduce(input_data)
+        self.assertEqual(actual_rows, expected_rows)
+
+    def _perform_reduce (self, input_data):
+        fh = self._create_csv(input_data)
+        report = tacl.Report(fh)
+        report.reduce()
+        return self._get_rows_from_csv(report.csv())
 
     def test_remove_label (self):
         input_data = (
