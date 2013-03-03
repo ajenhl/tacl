b312b81d7d76b5e4bd1dde09539d40ed8e4c3f1f
Jamie Norrish <jamie@artefact.org.nz>
2013-03-03 15:21:58 +1300

    Added more metadata to the setup script.

--- setup.py
+++ setup.py
@@ -8,7 +8,14 @@ setup(name='tacl',
       description='Text analyser for corpus linguistics',
       author='Jamie Norrish',
       author_email='jamie@artefact.org.nz',
+      url='https://github.com/ajenhl/tacl',
       packages=['tacl'],
       scripts=['bin/tacl', 'bin/tacl-helper'],
       requires=['lxml'],
+      classifiers=[
+        'Environment :: Console',
+        'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
+        'Programming Language :: Python :: 3',
+        'Topic :: Text Processing :: Linguistic',
+        ]
       )
