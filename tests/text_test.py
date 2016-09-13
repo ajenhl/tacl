a1c554f5689c13a5fe50e3e037bbc4b2c76d1475
Jamie Norrish <jamie@artefact.org.nz>
2016-09-14 08:50:51 +1200

    Simplified Text tests, removing redundancy.

--- tests/text_test.py
+++ tests/text_test.py
@@ -2,13 +2,71 @@
 
 import collections
 import unittest
-from unittest.mock import call, MagicMock, sentinel
 
 import tacl
 from .tacl_test_case import TaclTestCase
 
 
 class TextTestCase (TaclTestCase):
+
+    def setUp(self):
+        self._tokenizer = tacl.Tokenizer(
+            tacl.constants.TOKENIZER_PATTERN_CBETA,
+            tacl.constants.TOKENIZER_JOINER_CBETA)
+
+    def test_get_ngrams(self):
+        content = '阿闍世[(禾*尤)\n/上/日]首佛足。敬強阿闍世耶。又'
+        text = tacl.WitnessText('test', 'base', content, self._tokenizer)
+        expected_ngrams = [
+            (3, collections.Counter(
+                ['阿闍世', '闍世[(禾*尤)/上/日]', '世[(禾*尤)/上/日]首',
+                 '[(禾*尤)/上/日]首佛', '首佛足', '佛足敬', '足敬強',
+                 '敬強阿', '強阿闍', '阿闍世', '闍世耶', '世耶又'])),
+            (4, collections.Counter(
+                ['阿闍世[(禾*尤)/上/日]', '闍世[(禾*尤)/上/日]首',
+                 '世[(禾*尤)/上/日]首佛', '[(禾*尤)/上/日]首佛足',
+                 '首佛足敬', '佛足敬強', '足敬強阿', '敬強阿闍',
+                 '強阿闍世', '阿闍世耶', '闍世耶又']))
+        ]
+        actual_ngrams = list(text.get_ngrams(3, 4))
+        self.assertEqual(actual_ngrams, expected_ngrams)
+
+    def test_get_token_content_cbeta(self):
+        content = '阿闍世[(禾*尤)\n/上/日]首佛足。敬強阿闍世耶。又'
+        text = tacl.WitnessText('test', 'base', content, self._tokenizer)
+        expected_content = '阿闍世[(禾*尤)\n/上/日]首佛足敬強阿闍世耶又'
+        actual_content = text.get_token_content()
+        self.assertEqual(actual_content, expected_content)
+
+    def test_get_token_content_pagel(self):
+        content = "bka' stsal pa  | rigs kyi\nbu dag de'i || rigs kyi"
+        tokenizer = tacl.Tokenizer(tacl.constants.TOKENIZER_PATTERN_PAGEL,
+                                   tacl.constants.TOKENIZER_JOINER_PAGEL)
+        text = tacl.WitnessText('test', 'base', content, tokenizer)
+        expected_content = "bka' stsal pa rigs kyi bu dag de'i rigs kyi"
+        actual_content = text.get_token_content()
+        self.assertEqual(actual_content, expected_content)
+
+    def test_get_tokens_cbeta(self):
+        content = '阿闍世[(禾*尤)\n/上/日]首佛足。敬\n強耶。又'
+        text = tacl.Text(content, self._tokenizer)
+        expected_tokens = ['阿', '闍', '世', '[(禾*尤)\n/上/日]', '首', '佛',
+                           '足', '敬', '強', '耶', '又']
+        actual_tokens = text.get_tokens()
+        self.assertEqual(actual_tokens, expected_tokens)
+
+    def test_get_tokens_pagel(self):
+        content = "bka' stsal pa  | rigs kyi\nbu dag de'i || rigs kyi"
+        tokenizer = tacl.Tokenizer(tacl.constants.TOKENIZER_PATTERN_PAGEL,
+                                   tacl.constants.TOKENIZER_JOINER_PAGEL)
+        text = tacl.Text(content, tokenizer)
+        expected_content = ["bka'", 'stsal', 'pa', 'rigs', 'kyi', 'bu', 'dag',
+                            "de'i", 'rigs', 'kyi']
+        actual_content = text.get_tokens()
+        self.assertEqual(actual_content, expected_content)
+
+
+class WitnessTextTestCase (TaclTestCase):
 
     def setUp(self):
         self._tokenizer = tacl.Tokenizer(
@@ -39,58 +97,6 @@ class TextTestCase (TaclTestCase):
         actual_names = text.get_names()
         expected_names = ('T1', 'base')
         self.assertEqual(actual_names, expected_names)
-
-    def test_get_ngrams(self):
-        # Being a static method, a mock of tacl.Text._ngrams using
-        # autospec will be non-callable, so avoid this.
-        ngrams = self._create_patch('tacl.Text._ngrams', False)
-        sample_ngrams = ['a', 'b', 'c']
-        ngrams.return_value = sample_ngrams
-        get_tokens = self._create_patch('tacl.Text.get_tokens')
-        get_tokens.return_value = sentinel.tokens
-        collection = collections.Counter(sample_ngrams)
-        text = tacl.WitnessText('test', 'base', 'test content',
-                                self._tokenizer)
-        actual_ngrams = list(text.get_ngrams(2, 3))
-        expected_ngrams = [(2, collection), (3, collection)]
-        get_tokens.assert_called_once_with(text)
-        self.assertEqual(ngrams.mock_calls,
-                         [call(sentinel.tokens, 2), call(sentinel.tokens, 3)])
-        self.assertEqual(actual_ngrams, expected_ngrams)
-
-    def test_get_tokens(self):
-        content = '阿闍世[(禾*尤)/上/日]首佛足。敬\n強耶。又'
-        self._tokenizer.tokenize = MagicMock(return_value=sentinel.tokens)
-        text = tacl.WitnessText('test', 'base', content, self._tokenizer)
-        actual_tokens = text.get_tokens()
-        self._tokenizer.tokenize.assert_called_once_with(content)
-        self.assertEqual(actual_tokens, sentinel.tokens)
-
-    def test_ngrams_cbeta(self):
-        content = ''
-        text = tacl.WitnessText('test', 'base', content, self._tokenizer)
-        tokens = ['阿', '闍', '世', '[(禾*尤)\n/上/日]', '首', '佛', '足',
-                  '敬', '強', '耶', '又']
-        expected_ngrams = [
-            '阿闍世', '闍世[(禾*尤)/上/日]', '世[(禾*尤)/上/日]首',
-            '[(禾*尤)/上/日]首佛', '首佛足', '佛足敬', '足敬強', '敬強耶',
-            '強耶又'
-            ]
-        actual_ngrams = text._ngrams(tokens, 3)
-        self.assertEqual(expected_ngrams, actual_ngrams)
-
-    def test_ngrams_pagel(self):
-        content = ''
-        tokenizer = tacl.Tokenizer(tacl.constants.TOKENIZER_PATTERN_PAGEL,
-                                   tacl.constants.TOKENIZER_JOINER_PAGEL)
-        text = tacl.WitnessText('test', 'base', content, tokenizer)
-        tokens = ["dpa'", "sems", "dpa'", "chen", "po", "rnam", "par", "mi"]
-        expected_ngrams = [
-            "dpa' sems dpa'", "sems dpa' chen", "dpa' chen po", "chen po rnam",
-            "po rnam par", "rnam par mi"
-        ]
-        actual_ngrams = text._ngrams(tokens, 3)
-        self.assertEqual(expected_ngrams, actual_ngrams)
 
 
 if __name__ == '__main__':
