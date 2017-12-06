08fcf72bb2cd1420d31c48e294e3dbab3145de9d
Jamie Norrish <jamie@artefact.org.nz>
2017-12-07 11:20:39 +1300

    Reworked doc generation to fix newly occurring autodoc problem.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -14,16 +14,12 @@
 import os
 import sys
 
-import unittest.mock
-
-MOCK_MODULES = ['lxml']
-for mod_name in MOCK_MODULES:
-    sys.modules[mod_name] = unittest.mock.Mock()
-
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
-#sys.path.insert(0, os.path.abspath('.'))
+sys.path.insert(0, os.path.abspath('../..'))
+
+autodoc_mock_imports = ['colorlog', 'lxml', 'pandas']
 
 # -- General configuration -----------------------------------------------------
 
